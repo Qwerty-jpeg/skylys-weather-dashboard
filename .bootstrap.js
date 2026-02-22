c680d5aac0c4300232731b045059b6a65513f5dd
Aurelia <aurelia-bot@aurelia.com>
2026-02-22 09:57:39 +0000

    chore: Update bootstrap script with latest commands

--- .bootstrap.js
+++ .bootstrap.js
@@ -77,7 +77,9 @@ function updateWranglerJsonc() {
 }
 
 function runSetupCommands() {
-    const commands = [];
+    const commands = [
+    "bun install recharts lucide-react date-fns clsx tailwind-merge framer-motion"
+];
     
     if (commands.length === 0) {
         console.log('⊘ No setup commands to run');
